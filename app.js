020a1dfef5fe98fa479ceb7179a0d743d097c66f
namolla0710 <namolla2125@naver.com>
2025-05-06 14:32:04 +0900

    app.js 업데이트5

--- app.js
+++ app.js
@@ -40,7 +40,8 @@ function requestNalssi(year, month, day, hour, min, x, y, res){
   var url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst';
 
 
-  axios.get(url, {
+  axios.get({
+    url: url,
     params: {
       serviceKey: 'HTYEX8TzfOhfIF4IZVtuOMkwVOn/keASj8phfIrl6ckqqYuDr8diWCoXsPRFQvvV3IRJeXvt/wUVca+nRUgiDQ==',
       pageNo: '1',
